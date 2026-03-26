e3af6405c386586c9af181e9062e63028740e1e1
Mikey Ilagan <ilagan@gmail.com>
2026-03-26 13:26:37 -0400

    fix console error by moving file for webmanifest

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/fonts");
   eleventyConfig.addPassthroughCopy({ "src/static": "/" });
+  eleventyConfig.addPassthroughCopy({ "manifest.webmanifest": "manifest.webmanifest" });
 
   eleventyConfig.addGlobalData("site", {
     name: "LC Education Consulting",
